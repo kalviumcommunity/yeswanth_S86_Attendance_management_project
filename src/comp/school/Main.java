7015ddf57adc3c8dfc318483db3d03c6d740bd5b
Yeswanthpola05 <yeswanth.pola.s86@kalvium.community>
2025-08-02 11:00:18 +0530

    session-2

--- src/comp/school/Main.java
+++
@@ -1,8 +0,0 @@
-package comp.school;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the School Attendance System Project!");
-        System.out.println("Session 1: Project Setup and Orientation Complete.");
-    }
-}
